d82ae0dfc1236b64e238ff6b224833e3f7c5f6ff
Monika Jaskolka <bialy.monika@gmail.com>
2016-11-24 17:04:11 +0000

    Added to comments.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/forFCA/McMaster_Tools/Signature@8578 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/ImposedData.m
+++ src/ImposedData.m
@@ -1,21 +1,25 @@
 function [tagDex, dsDex] = ImposedData(address)
-% IMPOSEDDATA Get the imposed component of the signature.
+% IMPOSEDDATA Find the available scoped Gotos and Data Stores that are higher in the 
+%	hierarchy and are to be in the signature.
 %
 %   Inports:
 %		address     Simulink system path.
 %
 %   Outports:
-%       tagDex
-%       dsDex
+%       tagDex		Names of Gotos in scope in scope of the system.
+%       dsDex		Names of Data Store Memorys in scope of the system.
 
     tagDex  = {};
     dsDex   = {};
     
+    % Get blocks that are above this system
 	allBlocks = find_system(address, 'SearchDepth', 1);
 	allBlocks = setdiff(allBlocks, address);
     
 	for z = 1:length(allBlocks)
 		BlockType = get_param(allBlocks{z}, 'BlockType');
+
+		% Get names of Data Stores and Gotos
         if strcmp(BlockType, 'DataStoreMemory')
 			dsDex{end + 1} = get_param(allBlocks{z}, 'DataStoreName');
         elseif strcmp(BlockType, 'GotoTagVisibility')
