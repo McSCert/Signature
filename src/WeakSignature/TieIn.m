b3fd3af7ae7271d3bed1cf166dc0fc167ff71471
Bennett Mackenzie <mackeb@mcmaster.ca>
2017-05-26 15:58:38 +0000

    Fixing labels for subsections not being aligned properly given bandaid solution for resolution
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/forFCA/McMaster_Tools/Signature@10186 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/WeakSignature/TieIn.m
+++ src/WeakSignature/TieIn.m
@@ -36,7 +36,7 @@ function TieIn(address, num, scopeGotoAdd, scopeFromAdd, dataStoreWriteAdd,...
     % Constants: 
     FONT_SIZE = getSignatureConfig('heading_size', 12); % Heading font size
     FONT_SIZE_LARGER = str2num(FONT_SIZE) + 2;
-    X_OFFSET_HEADING = 150;
+    X_OFFSET_HEADING = 75;
     Y_OFFSET = 25;  % Vertical spacing between signature sections
     
     verticalOffset = 30;
